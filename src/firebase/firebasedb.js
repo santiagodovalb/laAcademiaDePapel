b74cecbd819c269373ce00ccf2c69d6de0af1a4d
santikundera <santikundera@gmail.com>
2021-11-19 14:52:38 -0300

    seguridad

--- src/firebase/firebasedb.js
+++ src/firebase/firebasedb.js
@@ -1,16 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+const {REACT_APP_API_KEY, REACT_APP_API_ID} = process.env
 // Follow this pattern to import other Firebase services
 // import { } from 'firebase/<service>';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDrVniKxaskBE4AwBcoqscMGtDIf3LpyQQ",
+    apiKey: REACT_APP_API_KEY,
     authDomain: "la-academia-de-papel.firebaseapp.com",
     projectId: "la-academia-de-papel",
     storageBucket: "la-academia-de-papel.appspot.com",
     messagingSenderId: "784790586628",
-    appId: "1:784790586628:web:c62a5cde30923460c77c51"
+    appId: REACT_APP_API_ID
 };
 
 const app = initializeApp(firebaseConfig);
